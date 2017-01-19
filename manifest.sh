b3e757fa6925ab4340cdb097072c22d39fa973b5
Jiri Mikulasek <jiri.mikulasek@gooddata.com>
2017-01-19 17:47:19 +0100

    upgrade to wiremock 2.5.0

--- manifest.sh
+++ manifest.sh
@@ -1,2 +1,2 @@
-ARTIFACT_URL=http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/2.0.10-beta/wiremock-standalone-2.0.10-beta.jar
+ARTIFACT_URL=http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/2.5.0/wiremock-standalone-2.5.0.jar
 DOWNLOAD_FROM=public
